becc4509d3aa939ba683740e78a91b4d80c2b0cd
Anish Tiwari <anish.at.tiwari@gmail.com>
2023-08-20 00:09:46 +0530

    feat: added onBoarding design

--- src/app/(auth)/onboarding/page.tsx
+++ src/app/(auth)/onboarding/page.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
+
+import AccountProfile from '@/components/forms/AccountProfile';
 const OnBoarding = () => {
   return (
-    <div>
-      <div className='text-white'>page</div>
+    <div className='mx-auto flex max-w-3xl flex-col justify-start px-10 py-20'>
+      <h1>Onboarding</h1>
+      <p className='py-4'>Complete your profile to use weave whisper</p>
+      <div className='bg-[#121417] p-10'>
+        <AccountProfile />
+      </div>
     </div>
   );
 };
